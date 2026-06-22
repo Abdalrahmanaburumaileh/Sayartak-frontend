4f37940d88266d7c5d61e1a42b66633e0812f9be
aburumaileh <aburumaileh04@gmail.com>
2026-06-22 22:05:55 +0300

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="sayartak-footer bg-dark text-light text-center py-3 border-top border-secondary">
+      <div className="container">
+        <p className="mb-0 small">
+          &copy; 2026, designed by Abdalrahman Aburumaileh
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
